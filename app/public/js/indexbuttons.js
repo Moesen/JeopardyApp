6698f2aa9214ee2019f89d298bd5d4c53404dbdf
Gustav Moesmand <gustavmoesmand@gmail.com>
2021-12-19 18:07:17 +0100

    added some more

---
+++ app/public/js/indexbuttons.js
@@ -0,0 +1,12 @@
+/* ======== JOIN BUTTON ACTIONS ========== */
+join_button = document.getElementById("join-button")
+join_button.addEventListener("click", () => {
+    console.log("test")
+})
+
+/* ======== HOST BUTTON ACTIONS ========== */
+join_button = document.getElementById("host-button")
+
+/* ======== CREATE BUTTON ACTIONS ========== */
+join_button = document.getElementById("create-button")
+
